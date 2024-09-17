fe8b94a5984bf9f36643bf12991e97a84c2dafe5
jossai87 <60478605+jossai87@users.noreply.github.com>
2024-09-17 06:04:06 -0500

    Update invoke_agent.py

--- streamlit_app/invoke_agent.py
+++ streamlit_app/invoke_agent.py
@@ -1,4 +1,3 @@
-import boto3
 from boto3.session import Session
 from botocore.auth import SigV4Auth
 from botocore.awsrequest import AWSRequest
@@ -9,29 +8,20 @@ from requests import request
 import base64
 import io
 import sys
-from PIL import Image
 
 #For this to run on a local machine in VScode, you need to set the AWS_PROFILE environment variable to the name of the profile/credentials you want to use. 
-#You also need to input your model ID near the bottom of this file.
 
 #check for credentials
 #echo $AWS_ACCESS_KEY_ID
 #echo $AWS_SECRET_ACCESS_KEY
 #echo $AWS_SESSION_TOKEN
 
-agentId = "{AGENT ID}" #INPUT YOUR AGENT ID HERE
-agentAliasId = "{AGANT ALIAS ID}" # Hits draft alias, set to a specific alias id for a deployed version
+agentId = "<YOUR AGENT ID>" #INPUT YOUR AGENT ID HERE
+agentAliasId = "<YOUR ALIAS ID>" #INPUT YOUR ALIAS ID HERE
+theRegion = "us-west-2"
 
-s3 = boto3.client('s3')
-sts_client = boto3.client('sts')
-account_id = sts_client.get_caller_identity().get('Account')
-region = boto3.Session().region_name
-theRegion = region
-
-# Construct the S3 bucket name
-bucket_name = f"bedrock-agent-images-{account_id}-{region}"
-os.environ['S3_IMAGE_BUCKET'] = bucket_name
-object_name = 'the_image.png' 
+os.environ["AWS_REGION"] = theRegion
+region = os.environ.get("AWS_REGION")
 llm_response = ""
 
 def sigv4_request(
@@ -41,7 +31,7 @@ def sigv4_request(
     params=None,
     headers=None,
     service='execute-api',
-    region=region,
+    region=os.environ['AWS_REGION'],
     credentials=Session().get_credentials().get_frozen_credentials()
 ):
     """Sends an HTTP request signed with SigV4
@@ -76,6 +66,8 @@ def sigv4_request(
         headers=req.headers,
         data=req.body
     )
+    
+    
 
 def askQuestion(question, url, endSession=False):
     myobj = {
@@ -95,89 +87,13 @@ def askQuestion(question, url, endSession=False):
         },
         region=theRegion,
         body=json.dumps(myobj)
-    ) 
+    )
+    
     return decode_response(response)
 
 
-def delete_file_from_s3(bucket_name, object_name):
-    try:
-        s3.delete_object(Bucket=bucket_name, Key=object_name)
-        print(f"File {object_name} deleted successfully from bucket {bucket_name}.")
-    except Exception as e:
-        print(f"Error deleting file {object_name} from bucket {bucket_name}: {str(e)}")
 
 
-
-def resize_image(image):
-    """
-    Resize the image to a width of 300 pixels while maintaining aspect ratio.
-    
-    Parameters:
-    - image: PIL Image object.
-    
-    Returns:
-    A byte array of the resized image.
-    """
-    # Desired width
-    target_width = 300
-
-    # Calculate the new height to maintain aspect ratio
-    original_width, original_height = image.size
-    aspect_ratio = original_height / original_width
-    new_height = int(target_width * aspect_ratio)
-
-    # Resize the image
-    resized_image = image.resize((target_width, new_height), Image.ANTIALIAS)
-
-    # Convert the resized image to a byte array
-    img_byte_arr = io.BytesIO()
-    resized_image.save(img_byte_arr, format='PNG')
-    
-    return img_byte_arr.getvalue()
-
-
-def upload_image_to_s3(uploaded_file):
-    """
-    Converts uploaded image to PNG format and uploads to an S3 bucket.
-    
-    Parameters:
-    - uploaded_file: The uploaded file object from Streamlit.
-    - bucket_name: The name of the S3 bucket.
-    
-    Returns:
-    A success message if upload succeeds, otherwise an error message.
-    """
-    # Initialize S3 client
-    s3_client = boto3.client('s3')
-    
-    # Supported image types
-    supported_types = ['jpg', 'jpeg', 'png']
-    file_type = uploaded_file.type.split('/')[-1]
-
-    if file_type.lower() not in supported_types:
-        return "File must be jpg, jpeg, or png."
-
-    try:
-        # Convert uploaded file to PIL Image then resize
-        image = Image.open(uploaded_file)
-
-        # Convert image to PNG byte array
-        img_byte_arr = io.BytesIO()
-
-        image.save(img_byte_arr, format='PNG')
-        img_byte_arr = img_byte_arr.getvalue()
-        #img_byte_arr = resize_image(img_byte_arr)
-
-        # Upload the PNG byte array to S3
-        s3_client.put_object(Body=img_byte_arr, Bucket=bucket_name, Key=image_name)
-
-        return "Image successfully uploaded to S3 as 'image.png'."
-    except Exception as e:
-        # Handle exceptions
-        print(e)
-        return "Failed to upload the image to S3."
-    
-    
 def decode_response(response):
     # Create a StringIO object to capture print statements
     captured_output = io.StringIO()
@@ -236,6 +152,7 @@ def decode_response(response):
 
 
 def lambda_handler(event, context):
+    
     sessionId = event["sessionId"]
     question = event["question"]
     endSession = False
@@ -253,7 +170,7 @@ def lambda_handler(event, context):
     
     try: 
         response, trace_data = askQuestion(question, url, endSession)
-
+        
         return {
             "status_code": 200,
             #"body": json.dumps({"response": response, "trace_data": trace_data})
